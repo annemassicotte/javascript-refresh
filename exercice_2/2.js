64de9443a3a210e45eaba237110ef34fee42712c
Anne Massicotte <anne@cinetic.ca>
2023-04-24 15:37:13 -0400

    Created function factorial

--- exercice_2/2.js
+++ exercice_2/2.js
@@ -14,4 +14,10 @@
 	5! = 5 × 4 × 3 × 2 × 1 = 120
 */
 
-const factorial = n => { }
+const factorial = n => { 
+	if (n === 0 || n === 1){
+		return 1;
+	} else{
+		return n * factorial (n - 1);
+	}
+}
