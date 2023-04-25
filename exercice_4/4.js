e08988ba31f836b45c5b2b2a9dacb26612d32a0d
Anne Massicotte <anne@cinetic.ca>
2023-04-25 09:53:48 -0400

    Created function countCharacters

--- exercice_4/4.js
+++ exercice_4/4.js
@@ -2,4 +2,10 @@
 	Écrivez une fonction qui prend en entrée une chaîne de caractères et retourne un objet avec la fréquence de chaque caractère. Ignorez les espaces, les caractères spéciaux et la casse (majuscules / minuscules).
 */
 
-const countCharacters = string => { }
+const countCharacters = string => {
+	let frequency = {};
+	string.toLowerCase().replace(/[^a-zA-Z]/g, '').split('').forEach(function (character) {
+		frequency[character] = (frequency[character] || 0) + 1;
+	});
+	return frequency;
+}
